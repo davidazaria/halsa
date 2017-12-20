5b496421de272e0b1ecdf628e37e04775818f4dd
= <azaria.david@gmail.com>
2017-12-20 15:51:20 -0500

    user migration sql written

--- db/migrations/users-migration.sql
+++ db/migrations/users-migration.sql
@@ -1,1 +1,16 @@
-users-migration.sql
+\c halsa_health
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  plan_id INTEGER REFERENCES plans,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  age BIGINT,
+  zip_code BIGINT,
+  household_size BIGINT,
+  income BIGINT
+);
+
+CREATE INDEX ON users (first_name);
