6c4b2180076b14b5167551fa9d98d50008c76939
= <azaria.david@gmail.com>
2017-12-20 19:30:24 -0500

    plans model change // commenting out

--- models/plans-model.js
+++ models/plans-model.js
@@ -2,7 +2,7 @@ const db = require('../config/dbConfig');
 
 const Plan = {};
 
-//need to verify whether we need a validator here
+//need to verify whether we need a validator
 
 Plan.findAll = () => {
   return db.many('SELECT * FROM plans');
