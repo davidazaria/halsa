34111179912304e2cf20ede3dc7098a550ec6a43
Yanina Tsutieva <ytsutieva@gmail.com>
2017-12-22 12:26:51 -0500

    very few changes

--- src/components/Users-list.jsx
+++ src/components/Users-list.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 
-import IceCream from './IceCream';
-import IceCreamForm from './IceCreamForm';
 
-const UsersList = (props) => {
-  return (
-    <div className="users-list">
-      {props.allIceCreams.map(icecream => {
-        return (props.currentlyEditing == icecream.id)
-          ? <IceCreamForm key={icecream.id} icecream={icecream} iceCreamSubmit={props.iceCreamSubmit} />
-          : <IceCream key={icecream.id} icecream={icecream} setEditing={props.setEditing} deleteIceCream={props.deleteIceCream} />
-      })}
+
+//get use
+const UsersList = () => {
+   return(
+   	<div className ="user-list">
+    <h3 >David Azaria </h3>
+      <div class="user-description">
+        <p>Plan </p>
+        <p>Zip </p>
+         <p> Income</p>
+        </div>
+
+
+         <span className="clickme" /*onClick={() => props.setEditing(props.user.id)}*/ >Edit</span>
+
     </div>
-  );
-};
+)
+}
 
+//for every id render sa user-list div
+ //create a user form page to edit saved inform
 export default UsersList;
--- src/components/Users-plan.jsx
+++ src/components/Users-plan.jsx
@@ -1,30 +1,30 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-const UserPlan = (props) => {
+const UserPlan = () => {
   return (
   	<div className = "selected-plan">
   	<h1>Congratulations! Here is your selected plan!</h1>
-     <div className="full-plan">
-        <h2 className="user-plan">{props.plans.plan_name}</h2>
-        <p>{props.plans.price_multiple}</p>
-        <p>{props.plans.in_network_deductible}</p>
-        <p>{props.plans.out_of_network_deductible}</p>
-        <p>{props.plans.maximum_out_of_pocket}</p>
-        <p>{props.plans.primary_care_copay}</p>
-        <p>{props.plans.specialist_copay}</p>
-        <p>{props.plans.urgent_care_copay}</p>
-        <p>{props.plans.generic_drugs}</p>
-        <p>{props.plans.brand_drugs}</p>
-        <p>{props.plans.concierge_access}</p>
-      </div>
-      <div className="buttons">
-
-        <Link className="button1" to="/">Get new quote </Link>
-        <Link className="button2" to="/">Choose another plan</Link>
-
     </div>
+     <div className="full-plan">
+        <h2 className="user-plan"></h2>
+        <p></p>
+        <p></p>
+        <p></p>
+
+        <p></p>
+      </div>
+
+ {/* add get form for new quote*/}
+      {/* add get plan from planlist*/}
+ <div className="buttons">
+        <Link className="button1" to="/">Get new quote</Link>
+
+        <Link className="button2" to="/">Choose another plan</Link>
+   </div>
+
 
   	)
-};
-export default UserPlan;
+}
+
+export default UserPlan
