9e7d4bc1407a91814c54c041dd63efd9ce4a5dc4
Daniel Graugnard <dangraug@me.com>
2017-12-26 17:41:08 -0500

    tidied up code

--- src/components/PlansList.jsx
+++ src/components/PlansList.jsx
@@ -13,15 +13,16 @@ class PlansList extends Component {
     //BIND YOUR METHODS!!
   }
 
-  //take zip from user, interpolate into api url
-  //Return object from zip code api
-  //In set state, collect State abbreviation from json object
-  //In lists component, plug in US states names into each health plan component
+  //  take zip from user, interpolate into api url
+  //  Return object from zip code api
+  //  In set state, collect State abbreviation from json object
+  //  In lists component, plug in US states names into each health plan component
 
-  //Make a function for each column in plansDB, or make a function for each health plan that takes multiple args:
-  //make api call (David's db)
-  //Collect price_multiple and..
-  //takes age from input, and multiplies by the price_multiple (depends on the plan).
+  //  Make a function for each column in plansDB,
+  //  or make a function for each health plan that takes multiple args:
+  //  make api call (David's db)
+  //  Collect price_multiple and..
+  //  takes age from input, and multiplies by the price_multiple (depends on the plan).
 
   priceMultiple(age) {
     fetch(/*David's API*/)
@@ -63,11 +64,11 @@ class PlansList extends Component {
     });
     return this.data * age;
   }
-  //If plan 1, method-for-plan A
+  //  If plan 1, method-for-plan A
   //If plan 1, method for-plan B
 
-  //Danny- Instead of using props in render here, pass in the function that will return you the result.
-  //You just need a method that multiplies the numbers in plans table, with AGE INPUT from form.
+  //  Danny- Instead of using props in render here, pass in the function that will return you the result.
+  //  You just need a method that multiplies the numbers in plans table, with AGE INPUT from form.
 
   render() {
     return(
@@ -84,7 +85,7 @@ class PlansList extends Component {
           <p>Generic Drugs: DATA </p>
           <p>Brand Drugs: DATA</p>
           <p>Concierge Acces: DATA </p>
-          <input type="submit" className="button" />
+          <input type="submit" placeholder="Select this plan!" className="button" />
         </div>
         <div className="plan-2">
           <h2>Halsa Health Bronze Choice 3000: DATA</h2>
@@ -98,7 +99,7 @@ class PlansList extends Component {
           <p>Generic Drugs: DATA </p>
           <p>Brand Drugs: DATA</p>
           <p>Concierge Acces: DATA </p>
-          <input type="submit" className="button" />
+          <input type="submit" placeholder="Select this plan!" className="button" />
         </div>
         <div className="plan-3">
           <h2>Halsa Health Bronze Choice 3000: DATA</h2>
@@ -112,7 +113,7 @@ class PlansList extends Component {
           <p>Generic Drugs: DATA </p>
           <p>Brand Drugs: DATA</p>
           <p>Concierge Acces: DATA </p>
-          <input type="submit" className="button" />
+          <input type="submit" placeholder="Select this plan!" className="button" />
         </div>
         <div className="plan-4">
           <h2>Halsa Health Bronze Choice 3000: DATA</h2>
@@ -126,7 +127,7 @@ class PlansList extends Component {
           <p>Generic Drugs: DATA </p>
           <p>Brand Drugs: DATA</p>
           <p>Concierge Acces: DATA </p>
-          <input type="submit" className="button" />
+          <input type="submit" placeholder="Select this plan!" className="button" />
         </div>
       </div>
     )
