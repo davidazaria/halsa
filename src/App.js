246c4c6d9b5f6fd82fe91f4e00315663b61a33cf
Daniel Graugnard <dangraug@me.com>
2017-12-27 16:55:31 -0500

    more stuff done

--- src/App.js
+++ src/App.js
@@ -6,23 +6,26 @@ import PlansList from './components/PlansList.jsx';
 import Header from './components/Header.jsx';
 // import UsersPlan from './components/UserPlan.jsx';
 import SelectedPlan from './components/SelectedPlan.jsx';
+import axios from 'axios'
 
 class App extends Component {
   constructor() {
     super();
-    this.setState = {
-      apiDataLoaded: false,
-      apiData: ''
+    this.state = {
+      plans: null,
+      apiDataLoaded: false
+
     };
     //  BIND YOUR METHODS HERE!!!
   }
 
+
   componentDidMount() {
-    fetch('http://localhost:3000/api/plans')
-      .then(res => res.json())
+    axios.get('http://localhost:3000/api/plans')
       .then(res => {
         this.setState({
-          apiDataLoaded: true,
+          plans: res.data.data.plans,
+          apiDataLoaded: true
         });
       }).catch(err => console.log(err));
   }
@@ -30,13 +33,32 @@ class App extends Component {
   //  Show logo and button here
   //  Click button to go the forms
 
+
+    //  take zip from user, interpolate into api url
+    //  Return object from zip code api
+    //  In set state, collect State abbreviation from json object
+    //  In lists component, plug in US states names into each health plan component
+
+    //  Make a function for each column in plansDB,
+    //  or make a function for each health plan that takes multiple args:
+    //  make api call (David's db)
+    //  Collect price_multiple and..
+    //  takes age from input, and multiplies by the price_multiple (depends on the plan).
+
+
+
+  //  Danny- Instead of using props in render here, pass in the function that will return you the result.
+  //  You just need a method that multiplies the numbers in plans table, with AGE INPUT from form.
+
   render() {
+    if (!this.state.plans) {
+      return (<p className="Loading">Loading...</p>)
+    }
     return (
       <div className="App">
         <main>
           <Header />
           <Form />
-          <PlansList />
           <SelectedPlan />
         </main>
       </div>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import PlansList from './PlansList';
+import axios from 'axios';
 
 //  Here is the the form component where users enter demographics
 class Form extends Component {
   constructor() {
     super();
     this.state = {
+      plans: null,
+      apiDataLoaded: false,
       name: '',
       age: '',
       zip: '',
@@ -12,6 +16,17 @@ class Form extends Component {
     };
     //  BIND METHODS!
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3000/api/plans')
+      .then(res => {
+        this.setState({
+          plans: res.data.data.plans,
+          apiDataLoaded: true
+        });
+      }).catch(err => console.log(err));
   }
 
   //  Takes the input from the Form and saves it to state
@@ -23,30 +38,38 @@ class Form extends Component {
     });
   }
 
+  handleSubmit(e) {
+    e.preventDefault();
+    (res => {
+      this.setState({})
+    })
+  }
+
   //  If !FormData, render Form
   //  Else, render PlansList
 
   render() {
+    if (!this.state.plans) {
+      return(<p className="Loading">Loading...</p>)
+    }
     return(
     <div className="user-form">
-
-      <form className={this.props.isAdd}>
+      <form className={this.props.isAdd} onSubmit={this.handleSubmit}>
       <div className="userflow1">
-        <input type="text" name="name" placeholder="name"  />
+        <input type="text" name="name" placeholder="name" />
         </div>
             <div className="userflow2">
-        <input type="text" name="age" placeholder="age" />
+        <input type="text" name="age" placeholder="age" onChange={this.handleInputChange}/>
            </div>
                <div className="userflow3">
         <input type="text" name="zip" placeholder="zip" />
         </div>
            <div className="userflow4">
-        <input type="text" name="income" placeholder="income" va />
+        <input type="text" name="income" placeholder="income" />
         </div>
-
-        <input type="submit" />
-
+        <input type="submit" name="submit" />
       </form>
+      <PlansList age={this.state.age} plansList={this.state.plans} />
     </div>
     );
   }
