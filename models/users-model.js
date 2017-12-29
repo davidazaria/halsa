48f47272701058a0e81a711450ed0ce42eef29f3
= <azaria.david@gmail.com>
2017-12-29 11:17:55 -0500

    incremental changes to users model to handle crud functionality

--- models/users-model.js
+++ models/users-model.js
@@ -1,51 +1,67 @@
 const db = require('../config/dbConfig');
 
-const usersDB = {};
+class UsersDB {
+  constructor(users) {
+    this.id = users.id || null;
+    this.username = this.__validate(users.username, 'username');
+    this.age = this.__validate(users.age, 'age');
+    this.zip_code = this.__validate(users.zip_code, 'zip_code');
+    this.plan_id = this.__validate(users.plan_id, 'plan_id');
+    this.income = this.__validate(users.income, 'income');
+  }
 
-usersDB.findAll = () => {
-  return db.many(`
-    SELECT *
-    FROM users u
-    ORDER BY u.username ASC`);
-};
+  __validate(property, propType) {
+    if (propType === 'age') {
+      const age = /^\d+$/;
+      if (!property.match(age)) throw new Error('invalid age');
+    }
+    if (property) return property;
+    else throw new Error(`Missing property ${propType}`);
+  }
 
-usersDB.findById = (id) => {
-  return db.one(`
+  static findAll() {
+    return db.manyOrNone('SELECT * FROM users u ORDER BY u.username ASC');
+  }
+
+  static findById(id) {
+    return db.one(`
     SELECT *
     FROM users u
     INNER JOIN plans p ON u.plan_id=p.id
     WHERE u.id = $1
-    `, id);
-};
+    `, id)
+      .then(users => new UsersDB(users));
+  }
 
-usersDB.save = (user) => {
-  return db.one(`
-    INSERT INTO users (plan_id, username, age, zip_code, income)
-    VALUES ($/plan_id/, $/username/, $/age/, $/zip_code/, $/income/)
-    RETRUNING *
-      `, user);
-};
+// usersDB.save = (user) => {
+//   return db.one(`
+//     INSERT INTO users (plan_id, username, age, zip_code, income)
+//     VALUES ($/plan_id/, $/username/, $/age/, $/zip_code/, $/income/)
+//     RETRUNING *
+//       `, user);
+// };
 
-usersDB.update = (user) => {
-  return db.one(`
-    UPDATE users
-    SET
-    plan_id = $/plan_id/,
-    username = $/username/,
-    age = $/age/,
-    zip_code = $/zip_code/,
-    income = $/income/
-    WHERE id = $/id/
-    RETRUNING *
-      `, user);
-};
+// usersDB.update = (user) => {
+//   return db.one(`
+//     UPDATE users
+//     SET
+//     plan_id = $/plan_id/,
+//     username = $/username/,
+//     age = $/age/,
+//     zip_code = $/zip_code/,
+//     income = $/income/
+//     WHERE id = $/id/
+//     RETRUNING *
+//       `, user);
+// };
 
-usersDB.destroy = (id) => {
-  return db.none(`
-    DELETE
-    FROM users
-    WHERE id = $1
-    `, id);
-};
+// usersDB.destroy = (id) => {
+//   return db.none(`
+//     DELETEs
+//     FROM users
+//     WHERE id = $1
+//     `, id);
+// };
+}
 
-module.exports = usersDB;
+module.exports = UsersDB;
