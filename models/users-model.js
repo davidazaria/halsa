691667eba9e09f8d49da803a293ec09e24e87697
= <azaria.david@gmail.com>
2017-12-29 12:13:30 -0500

    refactored users model to handle crud; removed conflicts from form.jsx file

--- models/users-model.js
+++ models/users-model.js
@@ -19,6 +19,13 @@ class UsersDB {
     else throw new Error(`Missing property ${propType}`);
   }
 
+  __modify(changes) {
+    for (let prop in changes) {
+      this.__validate(changes[prop], prop);
+    }
+    return Object.assign(this, changes);
+  }
+
   static findAll() {
     return db.manyOrNone('SELECT * FROM users u ORDER BY u.username ASC');
   }
@@ -33,35 +40,33 @@ class UsersDB {
       .then(users => new UsersDB(users));
   }
 
-// usersDB.save = (user) => {
-//   return db.one(`
-//     INSERT INTO users (plan_id, username, age, zip_code, income)
-//     VALUES ($/plan_id/, $/username/, $/age/, $/zip_code/, $/income/)
-//     RETRUNING *
-//       `, user);
-// };
+  static destroy(id) {
+    return db.none('DELETE FROM users WHERE id = $1', id);
+  }
 
-// usersDB.update = (user) => {
-//   return db.one(`
-//     UPDATE users
-//     SET
-//     plan_id = $/plan_id/,
-//     username = $/username/,
-//     age = $/age/,
-//     zip_code = $/zip_code/,
-//     income = $/income/
-//     WHERE id = $/id/
-//     RETRUNING *
-//       `, user);
-// };
+  save() {
+    return db.one(`
+      INSERT INTO users
+      (plan_id, username, age, zip_code, income)
+      VALUES ($/plan_id/, $/username/, $/age/, $/zip_code/, $/income/)
+      RETRUNING *
+        `, this).then(users => this.__modify(users));
+  }
 
-// usersDB.destroy = (id) => {
-//   return db.none(`
-//     DELETEs
-//     FROM users
-//     WHERE id = $1
-//     `, id);
-// };
+  update(changes) {
+    this.__modify(changes);
+    return db.one(`
+      UPDATE users
+      SET
+      plan_id = $/plan_id/,
+      username = $/username/,
+      age = $/age/,
+      zip_code = $/zip_code/,
+      income = $/income/
+      WHERE id = $/id/
+      RETRUNING *
+        `, this).then(users => this.__modify(users));
+  }
 }
 
 module.exports = UsersDB;
