f658a0c6f8df997d6d58ad23662e7d2870757571
= <azaria.david@gmail.com>
2018-01-02 09:14:33 -0500

    fixed users-list map issue

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -4,9 +4,10 @@ import User from './User';
 const UsersList = (props) => {
     return (
       <div className="users-list">
-        {props.usersList.map(users => {
+        {props.usersList.map((users, index) => {
           return (
             <User
+              key={index}
               username={users.username}
               plan_id={users.plan_id}
               age={users.age}
